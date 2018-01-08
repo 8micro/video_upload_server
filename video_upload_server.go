1ce357bef6c30ef989353363b8bb9e49afa17a03
cdmaji1 <cdmaji1@jd.com>
2018-01-08 20:45:58 +0800

    add code for parse video info

--- video_upload_server.go
+++ video_upload_server.go
@@ -9,11 +9,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
 var port = flag.Int("p", 8080, "Port number to listen to, defaults to 8080")
 var uploadDir = flag.String("d", "uploads", "Upload directory, defaults to 'uploads'")
+
+var ffprobePath = flag.String("ffprobe", "", "ffprobe path name")
+var ffmpegPath = flag.String("ffmpeg", "", "ffmpeg path name")
 
 // Request parameters
 const (
@@ -29,6 +34,7 @@ const (
 	paramTotalParts      = "qqtotalparts"     // total parts
 	paramFileName        = "qqfilename"       // file name for chunked requests
 	paramChunkSize       = "qqchunksize"      // size of the chunks
+	paramUserId          = "userid"
 )
 
 type UploadResponse struct {
@@ -37,6 +43,15 @@ type UploadResponse struct {
 	PreventRetry bool   `json:"preventRetry"`
 }
 
+type VideoInfo struct {
+	VideoWidth  uint32
+	VideoHeight uint32
+	Rate        uint64
+	Duration    float64
+	Size        uint64
+}
+
+//must have userid and prefix for saving uploading files
 func main() {
 	flag.Parse()
 	hostPort := fmt.Sprintf("0.0.0.0:%d", *port)
@@ -54,7 +69,7 @@ func UploadHandler(w http.ResponseWriter, req *http.Request) {
 		upload(w, req)
 		return
 	case http.MethodDelete:
-		delete(w, req)
+		//delete(w, req)
 		return
 	}
 	errorMsg := fmt.Sprintf("Method [%s] is not supported:", req.Method)
@@ -117,6 +132,33 @@ func delete(w http.ResponseWriter, req *http.Request) {
 
 }
 
+func executeFfprobeCommand(filename string, userId string, guid string) ([]byte, error) {
+	if len(*ffprobePath) == 0 {
+		log.Printf("ffprobe path is empty")
+		return nil, fmt.Errorf("parameter error")
+	}
+
+	filePathName := userId + "/" + guid + "/" + filename
+
+	args := fmt.Sprintf("-v error -show_format -show_streams -print_format flat %s", filePathName)
+
+	//ffprobe -v error -show_format -show_streams -print_format flat  test.mp4
+
+	cmd := exec.Command(*ffprobePath, args)
+
+	data, err := cmd.Output()
+	if err != nil {
+		log.Printf("execute command ffprobe %s  failed", args)
+		return nil, fmt.Errorf("execute command ffprobe %s  failed", args)
+	}
+
+	return data, nil
+}
+
+func parseFfprobeResult(result []byte) (*VideoInfo, error) {
+
+}
+
 func ChunksDoneHandler(w http.ResponseWriter, req *http.Request) {
 	if req.Method != http.MethodPost {
 		errorMsg := fmt.Sprintf("Method [%s] is not supported", req.Method)
@@ -124,6 +166,7 @@ func ChunksDoneHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	uuid := req.FormValue(paramUuid)
 	filename := req.FormValue(paramFileName)
+	userid := req.FormValue(paramUserId)
 	totalFileSize, err := strconv.Atoi(req.FormValue(paramTotalFileSize))
 	if err != nil {
 		writeHttpResponse(w, http.StatusInternalServerError, err)
@@ -167,6 +210,207 @@ func ChunksDoneHandler(w http.ResponseWriter, req *http.Request) {
 	if totalWritten != int64(totalFileSize) {
 		errorMsg := fmt.Sprintf("Total file size mistmatch, expected %d bytes but actual is %d", totalFileSize, totalWritten)
 		http.Error(w, errorMsg, http.StatusMethodNotAllowed)
+	} else {
+		data, err := executeFfprobeCommand(filename, userid, uuid)
+		if err != nil {
+			log.Printf("execute ffprobe command failed")
+			return
+		} else {
+			strData := string(data[:])
+			strLines := strings.Split(strData, "\n")
+			var videoInfo VideoInfo
+			//get info from these lines
+			/*
+				streams.stream.0.index=0
+				streams.stream.0.codec_name="h264"
+				streams.stream.0.codec_long_name="H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10"
+				streams.stream.0.profile="Main"
+				streams.stream.0.codec_type="video"
+				streams.stream.0.codec_time_base="1/30"
+				streams.stream.0.codec_tag_string="avc1"
+				streams.stream.0.codec_tag="0x31637661"
+				streams.stream.0.width=800
+				streams.stream.0.height=600
+				streams.stream.0.coded_width=800
+				streams.stream.0.coded_height=600
+				streams.stream.0.has_b_frames=2
+				streams.stream.0.sample_aspect_ratio="0:1"
+				streams.stream.0.display_aspect_ratio="0:1"
+				streams.stream.0.pix_fmt="yuv420p"
+				streams.stream.0.level=31
+				streams.stream.0.color_range="unknown"
+				streams.stream.0.color_space="unknown"
+				streams.stream.0.color_transfer="unknown"
+				streams.stream.0.color_primaries="unknown"
+				streams.stream.0.chroma_location="left"
+				streams.stream.0.field_order="unknown"
+				streams.stream.0.timecode="N/A"
+				streams.stream.0.refs=1
+				streams.stream.0.is_avc="true"
+				streams.stream.0.nal_length_size="4"
+				streams.stream.0.id="N/A"
+				streams.stream.0.r_frame_rate="15/1"
+				streams.stream.0.avg_frame_rate="15/1"
+				streams.stream.0.time_base="1/15"
+				streams.stream.0.start_pts=0
+				streams.stream.0.start_time="0.000000"
+				streams.stream.0.duration_ts=8002
+				streams.stream.0.duration="533.466667"
+				streams.stream.0.bit_rate="499122"
+				streams.stream.0.max_bit_rate="N/A"
+				streams.stream.0.bits_per_raw_sample="8"
+				streams.stream.0.nb_frames="8002"
+				streams.stream.0.nb_read_frames="N/A"
+				streams.stream.0.nb_read_packets="N/A"
+				streams.stream.0.disposition.default=1
+				streams.stream.0.disposition.dub=0
+				streams.stream.0.disposition.original=0
+				streams.stream.0.disposition.comment=0
+				streams.stream.0.disposition.lyrics=0
+				streams.stream.0.disposition.karaoke=0
+				streams.stream.0.disposition.forced=0
+				streams.stream.0.disposition.hearing_impaired=0
+				streams.stream.0.disposition.visual_impaired=0
+				streams.stream.0.disposition.clean_effects=0
+				streams.stream.0.disposition.attached_pic=0
+				streams.stream.0.disposition.timed_thumbnails=0
+				streams.stream.0.tags.creation_time="1970-01-01T00:00:00.000000Z"
+				streams.stream.0.tags.language="und"
+				streams.stream.0.tags.handler_name="VideoHandler"
+				streams.stream.1.index=1
+				streams.stream.1.codec_name="aac"
+				streams.stream.1.codec_long_name="AAC (Advanced Audio Coding)"
+				streams.stream.1.profile="LC"
+				streams.stream.1.codec_type="audio"
+				streams.stream.1.codec_time_base="1/44100"
+				streams.stream.1.codec_tag_string="mp4a"
+				streams.stream.1.codec_tag="0x6134706d"
+				streams.stream.1.sample_fmt="fltp"
+				streams.stream.1.sample_rate="44100"
+				streams.stream.1.channels=2
+				streams.stream.1.channel_layout="stereo"
+				streams.stream.1.bits_per_sample=0
+				streams.stream.1.id="N/A"
+				streams.stream.1.r_frame_rate="0/0"
+				streams.stream.1.avg_frame_rate="0/0"
+				streams.stream.1.time_base="1/44100"
+				streams.stream.1.start_pts=0
+				streams.stream.1.start_time="0.000000"
+				streams.stream.1.duration_ts=23535641
+				streams.stream.1.duration="533.688005"
+				streams.stream.1.bit_rate="96000"
+				streams.stream.1.max_bit_rate="96000"
+				streams.stream.1.bits_per_raw_sample="N/A"
+				streams.stream.1.nb_frames="22984"
+				streams.stream.1.nb_read_frames="N/A"
+				streams.stream.1.nb_read_packets="N/A"
+				streams.stream.1.disposition.default=1
+				streams.stream.1.disposition.dub=0
+				streams.stream.1.disposition.original=0
+				streams.stream.1.disposition.comment=0
+				streams.stream.1.disposition.lyrics=0
+				streams.stream.1.disposition.karaoke=0
+				streams.stream.1.disposition.forced=0
+				streams.stream.1.disposition.hearing_impaired=0
+				streams.stream.1.disposition.visual_impaired=0
+				streams.stream.1.disposition.clean_effects=0
+				streams.stream.1.disposition.attached_pic=0
+				streams.stream.1.disposition.timed_thumbnails=0
+				streams.stream.1.tags.creation_time="1970-01-01T00:00:00.000000Z"
+				streams.stream.1.tags.language="und"
+				streams.stream.1.tags.handler_name="SoundHandler"
+				format.filename="test.mp4"
+				format.nb_streams=2
+				format.nb_programs=0
+				format.format_name="mov,mp4,m4a,3gp,3g2,mj2"
+				format.format_long_name="QuickTime / MOV"
+				format.start_time="0.000000"
+				format.duration="533.688000"
+				format.size="39930706"
+				format.bit_rate="598562"
+				format.probe_score=100
+				format.tags.major_brand="isom"
+				format.tags.minor_version="512"
+				format.tags.compatible_brands="isomiso2avc1mp41"
+				format.tags.creation_time="1970-01-01T00:00:00.000000Z"
+				format.tags.encoder="Lavf53.24.2
+			*/
+			for index, ele := range strLines {
+				if strings.Contains(ele, ".width=") {
+					ele_value := strings.Split(ele, "=")
+					if len(ele_value) == 2 {
+						videoWidth, err := strconv.Atoi(ele_value[1])
+						if err != nil {
+							log.Printf("convert video width failed %v", ele_value[1])
+						} else {
+							videoInfo.VideoWidth = uint32(videoWidth)
+						}
+					} else {
+						log.Printf("read video width failed %v", ele)
+					}
+				}
+
+				if strings.Contains(ele, ".height=") {
+					ele_value := strings.Split(ele, "=")
+					if len(ele_value) == 2 {
+						videoHeight, err := strconv.Atoi(ele_value[1])
+						if err != nil {
+							log.Printf("convert video height failed %v", ele_value[1])
+						} else {
+							videoInfo.VideoHeight = uint32(videoHeight)
+						}
+					} else {
+						log.Printf("read video height failed %v", ele)
+					}
+				}
+
+				if strings.Contains(ele, ".duration=") {
+					ele_value := strings.Split(ele, "=")
+					if len(ele_value) == 2 {
+						duration, err := strconv.ParseFloat(ele_value[1], 64)
+						if err != nil {
+							log.Printf("convert video duration failed %v", ele_value[1])
+						} else {
+							videoInfo.Duration = duration
+						}
+					} else {
+						log.Printf("read video duration failed %v", ele)
+					}
+				}
+
+				if strings.Contains(ele, "format.bit_rate=") {
+					ele_value := strings.Split(ele, "=")
+					if len(ele_value) == 2 {
+						rate, err := strconv.ParseUint(ele_value[1], 10, 64)
+						if err != nil {
+							log.Printf("convert video bit rate failed %v", ele_value[1])
+						} else {
+							videoInfo.Rate = rate
+						}
+					} else {
+						log.Printf("read video bit rate failed %v", ele)
+					}
+				}
+
+				if strings.Contains(ele, "format.size=") {
+					ele_value := strings.Split(ele, "=")
+					if len(ele_value) == 2 {
+						size, err := strconv.ParseUint(ele_value[1], 10, 64)
+						if err != nil {
+							log.Printf("convert video bit rate failed %v", ele_value[1])
+						} else {
+							videoInfo.Size = size
+						}
+					} else {
+						log.Printf("read video bit rate failed %v", ele)
+					}
+				}
+
+			}
+
+			//todo call api to tell the videoinfo to db server
+
+		}
 	}
 }
 
